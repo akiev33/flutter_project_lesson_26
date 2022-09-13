9faa33fafac4a8c3e650af2f79039fa8795fc879
akiev33 <71421550+akiev33@users.noreply.github.com>
2022-09-13 18:55:20 +0600

    update

--- lib/screens/stack/stack_screen.dart
+++ lib/screens/stack/stack_screen.dart
@@ -16,189 +16,209 @@ class StackScreen extends StatelessWidget {
       body: Stack(
         children: [
           Image.asset(Images.shapeElement),
-          Column(
-            children: [
-              const SizedBox(height: 52),
-              Text('Главная', style: TextStyles.muller20w700),
-              const SizedBox(height: 20),
-              //
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 16),
-                padding: const EdgeInsets.only(
-                  left: 27,
-                  bottom: 21,
-                  right: 20,
-                  top: 18,
-                ),
-                decoration: BoxDecoration(
-                  color: AppColors.colorFFFFFF,
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset.zero,
-                      blurRadius: 14,
-                      color: AppColors.color45006F.withOpacity(0.35),
-                    ),
-                  ],
-                ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 52),
+                Text('Главная', style: TextStyles.muller20w700),
+                const SizedBox(height: 20),
                 //
-                child: Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 14,
-                        vertical: 12,
-                      ),
-                      decoration: const BoxDecoration(
-                        color: AppColors.colorFFD541,
-                        shape: BoxShape.circle,
-                      ),
-                      child: SvgPicture.asset(AppSvgs.thunderbolt),
-                    ),
-                    const SizedBox(width: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Начните зарабатывать!',
-                          style: TextStyles.muller15w700,
-                        ),
-                        const SizedBox(height: 7),
-                        Text(
-                          'Приобретите тариф Behype-PRO \nи начните свою карьеру!',
-                          style: TextStyles.muller10w400,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 44),
-              Container(
-                decoration: const BoxDecoration(
-                  color: AppColors.colorF9F8FF,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 16),
+                  padding: const EdgeInsets.only(
+                    left: 27,
+                    bottom: 21,
+                    right: 20,
+                    top: 18,
                   ),
-                ),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 19, vertical: 44),
-                height: 614,
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Категории',
-                      style: TextStyles.muller18w700,
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      height: 127,
-                      child: ListView.separated(
-                        //
-                        shrinkWrap: false,
-                        //
-                        separatorBuilder: (context, index) =>
-                            const SizedBox(width: 8.5),
-                        // physics: const NeverScrollableScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
-                        itemCount: category.length,
-                        itemBuilder: (context, index) => CategoriesItem(
-                          model: category[index],
-                        ),
+                  decoration: BoxDecoration(
+                    color: AppColors.colorFFFFFF,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset.zero,
+                        blurRadius: 14,
+                        color: AppColors.color45006F.withOpacity(0.35),
                       ),
-                    ),
-                    const SizedBox(height: 50),
-                    Row(
-                      children: [
-                        const SizedBox(width: 2),
-                        Text(
-                          'Рекламные кампании',
-                          style: TextStyles.muller18w700,
+                    ],
+                  ),
+                  //
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 14,
+                          vertical: 12,
                         ),
-                        const SizedBox(width: 102),
-                        TextButton(
-                          onPressed: () {},
-                          child: Container(
-                            height: 18,
-                            width: 38,
-                            decoration: BoxDecoration(
-                              color: AppColors.colorF90640,
-                              borderRadius: BorderRadius.circular(50),
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Все',
-                                style: TextStyles.muller12w500.copyWith(
-                                  color: AppColors.colorFFFFFF,
-                                ),
-                              ),
-                            ),
+                        decoration: const BoxDecoration(
+                          color: AppColors.colorFFD541,
+                          shape: BoxShape.circle,
+                        ),
+                        child: SvgPicture.asset(AppSvgs.thunderbolt),
+                      ),
+                      const SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Начните зарабатывать!',
+                            style: TextStyles.muller15w700,
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 34),
-                    Container(
-                      height: 124,
-                      width: 170,
-                      decoration: const BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset.zero,
-                            blurRadius: 10,
-                            color: AppColors.colorDED9FF,
+                          const SizedBox(height: 7),
+                          Text(
+                            'Приобретите тариф Behype-PRO \nи начните свою карьеру!',
+                            style: TextStyles.muller10w400,
                           ),
                         ],
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8),
-                          topRight: Radius.circular(8),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 44),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: const BoxDecoration(
+                    color: AppColors.colorF9F8FF,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 19, vertical: 44),
+                  // height: 614,
+
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Категории',
+                        style: TextStyles.muller18w700,
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        height: 127,
+                        child: ListView.separated(
+                          //
+                          shrinkWrap: false,
+                          //
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(width: 8.5),
+                          // physics: const NeverScrollableScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: category.length,
+                          itemBuilder: (context, index) => CategoriesItem(
+                            model: category[index],
+                          ),
                         ),
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            AppColors.colorD96DFF,
-                            AppColors.color6322E0,
+                      ),
+                      const SizedBox(height: 50),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Рекламные кампании',
+                            style: TextStyles.muller18w700,
+                          ),
+                          // const SizedBox(width: 102),
+                          // TextButton(
+                          //   onPressed: () {},
+                          //   child: Container(
+                          //     height: 18,
+                          //     width: 38,
+                          //     decoration: BoxDecoration(
+                          //       color: AppColors.colorF90640,
+                          //       borderRadius: BorderRadius.circular(50),
+                          //     ),
+                          //     child: Center(
+                          //       child: Text(
+                          //         'Все',
+                          //         style: TextStyles.muller12w500.copyWith(
+                          //           color: AppColors.colorFFFFFF,
+                          //         ),
+                          //       ),
+                          //     ),
+                          //   ),
+                          // ),
+                          SizedBox(
+                            height: 20,
+                            width: 45,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                primary: AppColors.colorF90640,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                              ),
+                              child: FittedBox(
+                                  child: Text('Все',
+                                      style: TextStyles.muller12w500)),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 34),
+                      DecoratedBox(
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: AppColors.colorDED9FF.withOpacity(0.33),
+                              blurRadius: 10,
+                              spreadRadius: 10,
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 124,
+                              width: 170,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 12),
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(8),
+                                ),
+                                gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    AppColors.color6322E0,
+                                    AppColors.colorD96DFF,
+                                  ],
+                                ),
+                              ),
+                              child: Image.asset(
+                                Images.pen,
+                                height: 110,
+                                width: 140,
+                              ),
+                            ),
+                            Container(
+                              height: 38,
+                              width: 170,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.vertical(
+                                  bottom: Radius.circular(8),
+                                ),
+                                color: AppColors.colorF9F8FF,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'В новом обновлении',
+                                  style: TextStyles.muller13w700,
+                                ),
+                              ),
+                            )
                           ],
                         ),
                       ),
-                      child: Image.asset(
-                        Images.pen,
-                        height: 110,
-                        width: 140,
-                      ),
-                    ),
-                    Container(
-                      height: 38,
-                      width: 170,
-                      decoration: const BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset.zero,
-                            blurRadius: 10,
-                            color: AppColors.colorDED9FF,
-                          ),
-                        ],
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(8),
-                          bottomRight: Radius.circular(8),
-                        ),
-                        color: AppColors.colorF9F8FF,
-                      ),
-                      child: Center(
-                        child: Text(
-                          'В новом обновлении',
-                          style: TextStyles.muller13w700,
-                        ),
-                      ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
